2600f56cd6f0f3e496cc599e2d20a6a0ab94e5ac
alexpopa <alex.florin.popa@gmail.com>
2015-04-05 12:20:48 -0400

    users created

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,19 @@ class CreateUsersTable extends Migration {
 		{
 			$table->increments('id');
 			$table->string('name');
+			$table->string('username')->unique();
 			$table->string('email')->unique();
 			$table->string('password', 60);
+			$table->integer('phone')->unique();
+			$table->date('birthday');
+			$table->enum('gender', ['M', 'F']);
+			$table->string('building');
+			$table->string('floor');
+			$table->string('apartment');
+			$table->string('job');
+			$table->string('hometown');
+			$table->string('aboutme');
+			$table->string('motto');
 			$table->rememberToken();
 			$table->timestamps();
 		});
@@ -30,7 +41,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users');
+		//Schema::drop('users');
 	}
 
 }

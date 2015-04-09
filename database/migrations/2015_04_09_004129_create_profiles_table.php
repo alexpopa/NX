12fbabd0347f4b2175abd1ed4dedb5013d4b3ddf
alexpopa <alex.florin.popa@gmail.com>
2015-04-08 20:46:50 -0400

    fixing users and profile backend

---
+++ database/migrations/2015_04_09_004129_create_profiles_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		$table->increments('id');
+        $table->integer('user_id')->unsigned();
+		$table->string('name');
+		$table->string('username')->unique();
+        $table->biginteger('uid')->unsigned();
+        $table->string('access_token');
+        $table->string('access_token_secret');
+		$table->integer('phone');
+		$table->date('birthday');
+		$table->enum('gender', ['M', 'F']);
+		$table->string('building');
+		$table->string('floor');
+		$table->string('apartment');
+		$table->string('job');
+		$table->string('hometown');
+		$table->string('aboutme');
+		$table->string('motto');
+		$table->string('access_token');
+        $table->string('access_token_secret');
+        $table->timestamps();
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('profiles');
+	}
+
+}

ebd0434c5d798405e0d4eb44f4d68bea58e7d21b
alexpopa <alex.florin.popa@gmail.com>
2015-04-21 18:47:32 -0400

    navbar css edits

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,11 +15,6 @@ Route::get('/', 'HomeController@showWelcome');
 
 Route::get('home', ['as' => 'home_path', 'uses' => 'HomeController@index']);
 
-Route::get('test', function()
-{
-    return 'Hello World';
-});
-
 /////////////////////////////////////////////////
 //
 //The Routes for any Users Activity
@@ -40,37 +35,37 @@ Route::controllers([
 ]);
 
 
-Route::get('login/fb', function() {
-    $facebook = new Facebook(Config::get('facebook'));
-    $params = array(
-        'redirect_uri' => url('/login/fb/callback'),
-        'scope' => 'email',
-    );
-    return Redirect::away($facebook->getLoginUrl($params));
-});
+//Route::get('login/fb', function() {
+//   $facebook = new Facebook(Config::get('facebook'));
+//   $params = array(
+//        'redirect_uri' => url('/login/fb/callback'),
+//        'scope' => 'email',
+//    );
+//    return Redirect::away($facebook->getLoginUrl($params));
+//});
 
-Route::get('login/fb/callback', function() {
-    $code = Input::get('code');
-    if (strlen($code) == 0) return Redirect::to('/')->with('message', 'There was an error communicating with Facebook');
-    $facebook = new Facebook(Config::get('facebook'));
-    $uid = $facebook->getUser();
-    if ($uid == 0) return Redirect::to('/')->with('message', 'There was an error');
-    $me = $facebook->api('/me');
-    $profile = Profile::whereUid($uid)->first();
-    if (empty($profile)) {
-        $user = new User;
-        $user->name = $me['first_name'].' '.$me['last_name'];
-        $user->email = $me['email'];
-        $user->photo = 'https://graph.facebook.com/'.$me['id'].'/picture?type=large';
-        $user->save();
-        $profile = new Profile();
-        $profile->uid = $uid;
-        $profile->username = $me['name'];
-        $profile = $user->profiles()->save($profile);
-    }
-    $profile->access_token = $facebook->getAccessToken();
-    $profile->save();
-    $user = $profile->user;
-    Auth::login($user);
-    return Redirect::to('/')->with('message', 'Logged in with Facebook');
-});
+//Route::get('login/fb/callback', function() {
+//    $code = Input::get('code');
+//    if (strlen($code) == 0) return Redirect::to('/')->with('message', 'There was an error communicating with Facebook');
+//    $facebook = new Facebook(Config::get('facebook'));
+//    $uid = $facebook->getUser();
+//    if ($uid == 0) return Redirect::to('/')->with('message', 'There was an error');
+//    $me = $facebook->api('/me');
+//    $profile = Profile::whereUid($uid)->first();
+//    if (empty($profile)) {
+//        $user = new User;
+//        $user->name = $me['first_name'].' '.$me['last_name'];
+//        $user->email = $me['email'];
+//        $user->photo = 'https://graph.facebook.com/'.$me['id'].'/picture?type=large';
+//        $user->save();
+//        $profile = new Profile();
+//        $profile->uid = $uid;
+//        $profile->username = $me['name'];
+//        $profile = $user->profiles()->save($profile);
+//    }
+//    $profile->access_token = $facebook->getAccessToken();
+//    $profile->save();
+//    $user = $profile->user;
+//    Auth::login($user);
+//    return Redirect::to('/')->with('message', 'Logged in with Facebook');
+//});
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,7 @@
 	<title>Laravel</title>
 
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/navbar.css') }}" rel="stylesheet">
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
@@ -19,7 +20,7 @@
 	<![endif]-->
 </head>
 <body>
-	<nav class="navbar nav-primary navbar-default">
+	<nav class="navbar navbar-default">
 		<div class="container-fluid">
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -28,12 +29,12 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Laravel</a>
+				<a class="navbar-brand" href="#"><img src="/NX/public/img/spotlogo_50.jpg"/></a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
-					<li><a href="{{ url('/') }}">Home</a></li>
+					<li><a href="{{ url('/') }}">SpotSPOC</a></li>
 				</ul>
 
 				<ul class="nav navbar-nav navbar-right">
